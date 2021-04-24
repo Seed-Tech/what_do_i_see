5de32393350f599b7e8c198e5b5abac3611d5fdb
torres1-23 <alejo.torres1247@gmail.com>
2021-04-24 03:42:49 +0000

    Add script to initializae a server locally

---
+++ backend/init_flask.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module inits a localhost server using flask, to
+send directory through http.
+
+Usage:
+    Execute it like this:
+    "python3 init_flask.py <path to image>"
+"""
+from flask import Flask
+from objdet import obj_detect
+import cv2
+import sys
+
+
+app = Flask(__name__)
+
+
+@app.rout
+def main():
+    """Return dictionary of image proccessed"""
+    return(obj_detect(cv2.imread(sys.argv[1])))
+
+if __name__ == '__main__':
+    app.run(debug=True)
